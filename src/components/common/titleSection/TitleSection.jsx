1633927a10864c46d09162e12de836dbc38482f8
Vetosy <xigga11@gmail.com>
2024-05-23 13:08:52 +0300

    add gsap srollTrigger

--- src/components/common/titleSection/TitleSection.jsx
+++ src/components/common/titleSection/TitleSection.jsx
@@ -1,18 +1,50 @@
 'use client'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { useGSAP } from '@gsap/react'
 import { useRef } from 'react'
 import classNames from 'classnames/bind'
 import styles from './TitleSection.scss'
 
+gsap.registerPlugin(ScrollTrigger)
 const cx = classNames.bind(styles)
 
 const TitleSection = ({ titleSection, modifier }) => {
-  const container = useRef()
+  const container = useRef(null)
+
+  // useGSAP(
+  //   () => {
+  //     gsap.fromTo(
+  //       container.current,
+  //       { opacity: 0, y: 40 },
+  //       {
+  //         opacity: 1,
+  //         y: 0,
+  //         duration: 0.5,
+  //         delay: 0.2,
+  //         scrollTrigger: {
+  //           trigger: container.current,
+  //           markers: true,
+  //           start: 'top 80%', // Когда верх элемента достигает 80% от высоты окна
+  //           end: 'bottom 20%', // Когда низ элемента достигает 20% от высоты окна
+  //           scrub: true // Анимация будет привязана к скроллу
+  //         }
+  //       }
+  //     )
+  //   },
+  //   { scope: container }
+  // )
 
   useGSAP(
     () => {
-      gsap.fromTo(container.current, { opacity: 0, y: 40 }, { opacity: 1, y: 0, duration: 0.5, delay: 0.2 })
+      gsap.to(container.current, {
+        scrollTrigger: {
+          trigger: container.current,
+          scrub: true,
+          markers: true,
+          x: 500
+        }
+      })
     },
     { scope: container }
   )
