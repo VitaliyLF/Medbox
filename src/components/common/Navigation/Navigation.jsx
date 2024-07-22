5187fffc8e85d1c92b555640c513bc6b2c8844fb
Vetosy <xigga11@gmail.com>
2024-07-22 10:47:50 +0300

    update header

--- src/components/common/Navigation/Navigation.jsx
+++ src/components/common/Navigation/Navigation.jsx
@@ -1,26 +1,59 @@
+'use client'
+
 import classNames from 'classnames'
 import Link from 'next/link'
+import { useState } from 'react'
 import Button from '../UI/Button/Button'
 import NavigationList from './NavigationList'
 
-const Navigation = ({ isMenuOpen, links, btnText, btnTextMobile }) => {
+const Navigation = ({ links, btnText, btnTextMobile }) => {
+  const [isMenuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenuVisibility = () => {
+    setMenuOpen(!isMenuOpen)
+    toggleScroll()
+  }
+
+  const toggleScroll = () => {
+    const paddingOffset =
+      window.innerWidth - document.documentElement.clientWidth
+    document.body.classList.toggle('scroll-lock')
+    document.body.style.paddingRight = isMenuOpen ? '' : `${paddingOffset}px`
+    document.body.style.scrollBehavior = isMenuOpen ? '' : 'unset'
+  }
+
   return (
-    <nav
-      className={classNames('nav', {
-        'nav--active': isMenuOpen,
-      })}
-      aria-label="Main navigation"
-      title="Use to navigation">
-      <NavigationList links={links} />
-      <div className="nav-mobile">
-        {Boolean(btnText) && <Button type="button">{btnText}</Button>}
-        {Boolean(btnTextMobile) && (
-          <Link className="btn btn--phone" href="tel:8663537856">
-            {btnTextMobile}
-          </Link>
-        )}
-      </div>
-    </nav>
+    <>
+      <nav
+        className={classNames('nav', {
+          'nav--active': isMenuOpen,
+        })}
+        aria-label="Main navigation"
+        title="Use to navigation">
+        <NavigationList links={links} />
+        <div className="nav-mobile">
+          {Boolean(btnText) && <Button type="button">{btnText}</Button>}
+          {Boolean(btnTextMobile) && (
+            <Link className="btn btn--phone" href="tel:8663537856">
+              {btnTextMobile}
+            </Link>
+          )}
+        </div>
+      </nav>
+      <Button
+        className={classNames('burger', {
+          'burger--active': isMenuOpen,
+        })}
+        type="button"
+        aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+        aria-expanded={isMenuOpen ? 'true' : 'false'}
+        onClick={toggleMenuVisibility}>
+        <span className="burger__info" aria-hidden="true">
+          {isMenuOpen ? 'Close menu' : 'Menu'}
+        </span>
+        <span className="burger__line"></span>
+      </Button>
+    </>
   )
 }
 export default Navigation
--- src/components/sections/Header/Header.jsx
+++ src/components/sections/Header/Header.jsx
@@ -1,5 +1,7 @@
+import Logotype from '@/components/common/Logotype/Logotype'
+import Navigation from '@/components/common/Navigation/Navigation'
 import ScrollToTop from '@/components/common/ScrollToTop/ScrollToTop'
-import HeaderWrapper from './HeaderWrapper'
+import Button from '@/components/common/UI/Button/Button'
 
 const Header = ({ headerContent }) => {
   const { logo, links, btnText, btnTextMobile } = headerContent ?? {}
@@ -7,12 +9,23 @@ const Header = ({ headerContent }) => {
   return (
     <header className="header" id="scroll-to-top">
       <div className="header__container container">
-        <HeaderWrapper
-          logo={logo}
-          links={links}
-          btnText={btnText}
-          btnTextMobile={btnTextMobile}
-        />
+        <div className="header__wrapper">
+          <Logotype
+            logoImage={logo}
+            logoClass="header__logo"
+            logoImageClass="header__logo-img"
+          />
+          <Navigation
+            links={links}
+            btnText={btnText}
+            btnTextMobile={btnTextMobile}
+          />
+          {Boolean(btnText) && (
+            <Button modifier="header" type="button">
+              {btnText}
+            </Button>
+          )}
+        </div>
       </div>
       <ScrollToTop />
     </header>
