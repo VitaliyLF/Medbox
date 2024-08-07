4a2dce9d909b95b33641785af34adf742cc11a35
VitaliyLF <97784759+Vetosy@users.noreply.github.com>
2024-08-07 22:10:42 +0300

    update

--- src/components/common/UI/Details/Details.jsx
+++ src/components/common/UI/Details/Details.jsx
@@ -1,21 +1,44 @@
+'use client'
+
 import { urlFor } from '@/app/lib/clientSanity'
 import { decodeAssetId } from '@/utils/sanityDecodeImg'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect, useRef } from 'react'
 import TextSection from '../../TextSection/TextSection'
 
 const Details = ({
   source: { summary, text, url, linkText, image, alt } = {},
   isOpen = false,
 }) => {
+  const detailsRef = useRef(null)
+
+  useEffect(() => {
+    const summaryElement = detailsRef.current.querySelector('summary')
+
+    const handleClick = () => {
+      if (detailsRef.current && !detailsRef.current.open) {
+        detailsRef.current.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+
+    summaryElement?.addEventListener('click', handleClick)
+
+    return () => {
+      summaryElement?.removeEventListener('click', handleClick)
+    }
+  }, [])
+
   const imageDimensions = image?.asset
     ? decodeAssetId(image.asset._ref).dimensions
     : null
 
   return (
-    <details className="details" open={isOpen}>
+    <details ref={detailsRef} className="details" open={isOpen}>
       {Boolean(summary) && (
-        <summary className="details__summary" title="Open/Close information">
+        <summary
+          className="details__summary"
+          title={isOpen ? 'Close information' : 'Open information'}>
           {summary}
         </summary>
       )}
